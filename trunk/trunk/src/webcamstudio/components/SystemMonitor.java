13d4711aa82c7fd23d074c0e7acb6f794a991387
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2011-04-22 09:08:03 +0000

    Updated average system load monitor

--- trunk/trunk/src/webcamstudio/components/SystemMonitor.java
+++ trunk/trunk/src/webcamstudio/components/SystemMonitor.java
@@ -34,7 +34,7 @@ public class SystemMonitor implements Runnable{
         while (!stopMe) {
             try {
                 bar.setValue((int) (bean.getSystemLoadAverage()*100));
-                System.out.println(bean.getSystemLoadAverage());
+                //System.out.println(bean.getSystemLoadAverage());
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(SystemMonitor.class.getName()).log(Level.SEVERE, null, ex);
