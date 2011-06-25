0a02279b7360d8a1d6ccf7e3fad4b83e5e4f6b99
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2011-06-25 23:17:33 +0000

    Reworked movie playing

--- trunk/src/webcamstudio/sources/VideoSourceMovie.java
+++ trunk/src/webcamstudio/sources/VideoSourceMovie.java
@@ -40,44 +40,46 @@ public class VideoSourceMovie extends VideoSource implements org.gstreamer.eleme
         captureWidth = 320;
         captureHeight = 240;
         doRescale = true;
+        keepRatio=true;
     }
 
     public VideoSourceMovie(java.io.File loc) {
 
         location = loc.getAbsolutePath();
         name = loc.getName();
-        colorspace = GST_COLORSPACE_YUV;
         hasSound = true;
         volume = 10;
         frameRate = 24;
         captureWidth = 320;
         captureHeight = 240;
         doRescale = true;
+        keepRatio=true;
     }
 
     public VideoSourceMovie(java.net.URL loc) {
 
         location = loc.toString();
         name = loc.toString();
-        colorspace = GST_COLORSPACE_YUV;
         hasSound = true;
         volume = 10;
         frameRate = 24;
         captureWidth = 320;
         captureHeight = 240;
         doRescale = true;
+        keepRatio=true;
     }
 
     public VideoSourceMovie(String loc) {
         location = loc;
         name = loc;
-        colorspace = GST_COLORSPACE_YUV;
+        
         hasSound = true;
         volume = 10;
         frameRate = 24;
         captureWidth = 320;
         captureHeight = 240;
         doRescale = true;
+        keepRatio=true;
     }
 
     public void setName(String n) {
@@ -99,7 +101,6 @@ public class VideoSourceMovie extends VideoSource implements org.gstreamer.eleme
             pipe = null;
         }
         elementSink = null;
-        elementSource = null;
         image = null;
         pixels = null;
         tempimage = null;
@@ -108,162 +109,79 @@ public class VideoSourceMovie extends VideoSource implements org.gstreamer.eleme
 
     @Override
     public void startSource() {
+        String pipeline = "";
         try {
             isPlaying = true;
             if (location.toLowerCase().startsWith("http://") || location.toLowerCase().startsWith("https://")) {
-                elementSource = ElementFactory.make(GST_SOUPHTTPSRC, GST_SOUPHTTPSRC + uuId);
-                elementSource.set("location", location);
+                pipeline = "souphttpsrc location=\""+location+"\" ! decodebin name=decode ";
+                loadSound=true;
             } else if (location.toLowerCase().startsWith("rtsp://")) {
-                elementSource = ElementFactory.make(GST_RTSPSRC, GST_RTSPSRC + uuId);
-                elementSource.set("location", location);
+                pipeline = "rtspsrc location=\""+location+"\" ! decodebin name=decode ";
                 loadSound = true;
             } else if (location.toLowerCase().startsWith("tcp://")) {
-                System.out.println("Connection with TCP");
-                elementSource = ElementFactory.make(GST_TCPCLIENTSRC, GST_TCPCLIENTSRC + uuId);
+                pipeline = "tcpclientsrc ";
+                
                 String[] addr = location.replaceFirst("tcp://", "").split(":");
                 switch (addr.length) {
                     case 2:
-                        elementSource.set("host", addr[0]);
-                        elementSource.set("port", new Integer(addr[1]).intValue());
+                        pipeline+= " host=" +addr[0];
+                        pipeline+= " port=" + addr[1];
                         break;
                     case 1:
-                        elementSource.set("host", addr[0]);
-                        elementSource.set("port", 4888);
+                        pipeline+= " host=" +addr[0];
+                        pipeline+= " port=4888";
                         break;
                 }
+                pipeline+= " ! decodebin  name=decode ";
+                loadSound = true;
             } else if (location.toLowerCase().startsWith("rfb://")) {
-                System.out.println("Connection with RFB");
-                elementSource = ElementFactory.make("rfbsrc", "rfbsrc" + uuId);
-                elementDecoder = null;
+
+                pipeline = "rfbsrc view-only=true incremental=false";
                 String[] addr = location.replaceFirst("rfb://", "").split(":");
-                frameRate = 5;
-                elementSource.set("view-only", true);
-                elementSource.set("incremental", false);
                 switch (addr.length) {
                     case 3:
-                        elementSource.set("host", addr[0]);
-                        elementSource.set("port", new Integer(addr[1]).intValue());
-                        elementSource.set("password", addr[2]);
+                        pipeline+= " host=" +addr[0];
+                        pipeline+= " port=" +addr[1];
+                        pipeline+= " password=" +addr[2];
                         break;
                     case 2:
-                        elementSource.set("host", addr[0]);
-                        elementSource.set("port", new Integer(addr[1]).intValue());
+                        pipeline+= " host=" +addr[0];
+                        pipeline+= " port=" +addr[1];
                         break;
                     case 1:
-                        elementSource.set("host", addr[0]);
-                        elementSource.set("port", 5900);
+                        pipeline+= " host=" +addr[0];
                         break;
                 }
                 loadSound = false;
             } else {
-                elementSource = ElementFactory.make(GST_FILESRC, GST_FILESRC + uuId);
-                elementSource.set("location", location);
+                pipeline="filesrc location=\""+location+"\" ! decodebin  name=decode ";
+                loadSound = true;
             }
 
-
-            elementAudioSink = ElementFactory.make(audioSink, name + " (" + uuId + ")");
-
+            pipeline += " ! ffmpegcolorspace ";
             if (outputHeight == 0 && outputWidth == 0) {
                 outputWidth = captureWidth;
                 outputHeight = captureHeight;
             }
-            setOpacity(opacity);
-            pipe = new Pipeline("WebcamStudio-" + uuId);
+            if (doRescale){
+                pipeline += " ! videorate ! video/x-raw-yuv,framerate="+frameRate + "/1 ! videoscale add-borders=true ! video/x-raw-yuv,width=" + captureWidth + ",height=" + captureHeight;
+            }
+            if (activeEffect.length() != 0) {
+                pipeline += " ! ffmpegcolorspace ! " + activeEffect + " ! ffmpegcolorspace ";
+            }
+            pipeline+= " ! alpha ! ffmpegcolorspace ! video/x-raw-rgb,bpp=32,depth=24, red_mask=65280, green_mask=16711680, blue_mask=-16777216 ! ffmpegcolorspace name=tosink";
+            if (loadSound){
+                pipeline += " decode. ! queue ! volume name=volume volume="+(double)volume / 100D +" ! audioconvert ! autoaudiosink ";
+            }
+            System.out.println(pipeline);
+            pipe = Pipeline.launch(pipeline);
             elementSink = new org.gstreamer.elements.RGBDataSink("RGBDataSink" + uuId, this);
-            elementSink.setPassDirectBuffer(true);
-            //Adding elements to pipe
-            pipe.add(elementQueueVideo);
-            pipe.add(elementSource);
-            if (elementDecoder != null) {
-                pipe.add(elementDecoder);
-                if (loadSound) {
-                    pipe.add(elementQueueAudio);
-                    pipe.add(elementAudioSink);
-                    elementAudioVolume.set("volume", (double) volume / 100D);
-                    pipe.add(elementAudioVolume);
-                    pipe.add(elementAudioRate);
-                    pipe.add(elementAudioConvert);
-                }
-            }
-            pipe.add(elementFFMpegColorspace1);
-            pipe.add(elementFFMpegColorspace2);
-            this.effectSource = elementFFMpegColorspace2;
-            this.effectSink = elementFFMpegColorspace2;
-
+            elementSink.setPassDirectBuffer(false);
             pipe.add(elementSink);
-
-            // Linking everything together
-
-            if (elementDecoder != null && elementDecoder instanceof org.gstreamer.elements.DecodeBin) {
-                elementSource.link(elementDecoder);
-                ((org.gstreamer.elements.DecodeBin) elementDecoder).connect(new org.gstreamer.elements.DecodeBin.NEW_DECODED_PAD() {
-
-                    public void newDecodedPad(Element elem, Pad pad, boolean last) {
-                        /* only link once */
-                        if (pad.isLinked()) {
-                            return;
-                        }
-                        /* check media type */
-                        Caps caps = pad.getCaps();
-                        Structure struct = caps.getStructure(0);
-                        if (struct.getName().startsWith("audio/") && loadSound) {
-                            System.out.println("Audio pad: " + struct.getName());
-                            pad.link(elementQueueAudio.getStaticPad("sink"));
-                            elementQueueAudio.link(elementAudioRate);
-                            elementAudioRate.link(elementAudioVolume);
-                            elementAudioVolume.link(elementAudioConvert);
-                            elementAudioConvert.link(elementAudioSink);
-                        } else if (struct.getName().startsWith("video/")) {
-                            System.out.println("Linking video pad: " + struct.getName());
-                            pad.link(elementQueueVideo.getStaticPad("sink"));
-                            elementQueueVideo.link(elementFFMpegColorspace1);
-                        } else {
-                            System.out.println("Unknown pad [" + struct.getName() + "]");
-                        }
-                    }
-                });
-            } else if (elementDecoder != null) {
-                elementSource.link(elementDecoder);
-                elementDecoder.link(elementQueueVideo);
-                elementQueueVideo.link(elementFFMpegColorspace1);
-            } else {
-                elementSource.link(elementFFMpegColorspace1);
-            }
-
-            if (doRescale) {
-                Element videorate = ElementFactory.make("videorate", "videorate" + uuId);
-                Element videorateCaps = ElementFactory.make("capsfilter", "videoratecaps" + uuId);
-                videorateCaps.setCaps(Caps.fromString("video/x-raw-yuv,framerate=" + frameRate + "/1"));
-                Element videoscale = ElementFactory.make("videoscale", "videoscale" + uuId);
-                Element videoscaleCaps = ElementFactory.make("capsfilter", "videoscalecaps" + uuId);
-                videoscaleCaps.setCaps(Caps.fromString("video/x-raw-yuv,width=" + captureWidth + ",height=" + captureHeight));
-                pipe.add(videorate);
-                pipe.add(videorateCaps);
-                pipe.add(videoscale);
-                pipe.add(videoscaleCaps);
-                elementFFMpegColorspace1.link(videoscale);
-                videoscale.link(videoscaleCaps);
-                videoscaleCaps.link(videorate);
-                videorate.link(videorateCaps);
-                videorateCaps.link(elementFFMpegColorspace2);
-            } else {
-                elementFFMpegColorspace1.link(elementFFMpegColorspace2);
-            }
-            activateEffect(pipe);
-            Element sinkCaps = ElementFactory.make("capsfilter", "sinkCaps" + uuId);
-            sinkCaps.setCaps(Caps.fromString("video/x-raw-rgb,bpp=32,depth=24, red_mask=65280, green_mask=16711680, blue_mask=-16777216"));
-            pipe.add(sinkCaps);
-            if (currentEffect != null) {
-                pipe.add(currentEffect);
-                pipe.add(elementFFMpegColorspace3);
-                elementFFMpegColorspace2.link(currentEffect);
-                currentEffect.link(elementFFMpegColorspace3);
-                elementFFMpegColorspace3.link(sinkCaps);
-                sinkCaps.link(elementSink);
-            } else {
-                elementFFMpegColorspace2.setCaps(Caps.fromString("video/x-raw-rgb,bpp=32,depth=24, red_mask=65280, green_mask=16711680, blue_mask=-16777216"));
-                elementFFMpegColorspace2.link(sinkCaps);
-                sinkCaps.link(elementSink);
+            Element lastElement = pipe.getElementByName("tosink");
+            lastElement.link(elementSink);
+            if (loadSound){
+                elementAudioVolume = pipe.getElementByName("volume");
             }
             pipe.getBus().connect(new Bus.EOS() {
 
@@ -378,14 +296,7 @@ public class VideoSourceMovie extends VideoSource implements org.gstreamer.eleme
         return retValue;
     }
 
-    public void activateEffect(Pipeline pipe) {
-        if (activeEffect.length() == 0) {
-            currentEffect = null;
-        } else {
-            currentEffect = ElementFactory.make(activeEffect, activeEffect + uuId);
-        }
-    }
-
+    
     @Override
     public String toString() {
         return "Movie: " + new java.io.File(location).getName();
@@ -408,39 +319,11 @@ public class VideoSourceMovie extends VideoSource implements org.gstreamer.eleme
         list.add(new ControlAudio(this));
         return list;
     }
-    private static final String GST_FFMPEGCOLORSPACE = "ffmpegcolorspace";
-    private static final String GST_FILESRC = "filesrc";
-    private static final String GST_SOUPHTTPSRC = "souphttpsrc";
-    private static final String GST_RTSPSRC = "rtspsrc";
-    private static final String GST_TCPCLIENTSRC = "tcpclientsrc";
-    private static final String GST_VIDEORATE = "videorate";
-    private static final String GST_VIDEOSCALE = "videoscale";
-    private static final String GST_CAPSFILTER = "capsfilter";
-    protected static final String GST_DECODEBIN = "decodebin";
-    private static final String GST_VIDEOBALANCE = "videobalance";
-    private static final String GST_GAMMA = "gamma";
-    protected static final String GST_COLORSPACE_YUV = "video/x-raw-yuv";
-    protected static final String GST_COLORSPACE_RGB = "video/x-raw-rgb";
-    private static final String GST_QUEUE = "queue";
-    private static final String GST_VOLUME = "volume";
-    private static final String GST_AUDIORATE = "audiorate";
-    private static final String GST_AUDIOCONVERT = "audioconvert";
-    protected String colorspace = GST_COLORSPACE_YUV;
     private Element effectSource = null;
     private Element currentEffect = null;
     private Element effectSink = null;
-    protected Element elementSource = null;
-    protected Element elementDecoder = new org.gstreamer.elements.DecodeBin(GST_DECODEBIN + uuId);
+    private Element elementAudioVolume = null;
     private org.gstreamer.elements.RGBDataSink elementSink = null;
-    private Element elementFFMpegColorspace1 = ElementFactory.make(GST_FFMPEGCOLORSPACE, "ffmpegcolorspace1" + uuId);
-    private Element elementFFMpegColorspace2 = ElementFactory.make(GST_FFMPEGCOLORSPACE, "ffmpegcolorspace2" + uuId);
-    private Element elementFFMpegColorspace3 = ElementFactory.make(GST_FFMPEGCOLORSPACE, "ffmpegcolorspace3" + uuId);
-    protected Element elementAudioSink = null;
-    private Element elementQueueAudio = ElementFactory.make(GST_QUEUE, GST_QUEUE + "A_" + uuId);
-    private Element elementQueueVideo = ElementFactory.make(GST_QUEUE, GST_QUEUE + "V_" + uuId);
-    private Element elementAudioRate = ElementFactory.make(GST_AUDIORATE, GST_AUDIORATE + uuId);
-    private Element elementAudioVolume = ElementFactory.make(GST_VOLUME, GST_VOLUME + uuId);
-    private Element elementAudioConvert = ElementFactory.make(GST_AUDIOCONVERT, GST_AUDIOCONVERT + uuId);
     private Pipeline pipe = null;
     private long duration = 0;
 
