542b07007d857c5bd09ae0ae6c4539257db450fd
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2011-08-28 01:15:37 +0000

    Fixed mirror output for Google hanghout

--- trunk/src/webcamstudio/exporter/vloopback/V4L2Loopback.java
+++ trunk/src/webcamstudio/exporter/vloopback/V4L2Loopback.java
@@ -7,21 +7,25 @@ package webcamstudio.exporter.vloopback;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
-import java.awt.image.BufferedImage;
 import webcamstudio.InfoListener;
 
 /**
  *
  * @author patrick
  */
-public class V4L2Loopback extends VideoOutput{
+public class V4L2Loopback extends VideoOutput {
 
+    int width = 0;
+    int height = 0;
 
-    public V4L2Loopback(InfoListener l){
-        listener=l;
+    public V4L2Loopback(InfoListener l) {
+        listener = l;
     }
+
     @Override
     public void open(String path, int w, int h, int pixFormat) {
+        width = w;
+        height = h;
         devicePath = path;
         this.pixFormat = pixFormat;
         devFD = CV4l2.INSTANCE.open_device(devicePath, w, h, pixFormat);
@@ -37,14 +41,19 @@ public class V4L2Loopback extends VideoOutput{
 
     @Override
     public void write(int[] data) {
-//        if (flipImage){
-//            bi = flipper.cloneImage(bi);
-//            flipper.applyEffect(bi);
-//        }
+        if (flipImage) {
+            int[] buffer = new int[data.length];
+            for (int y = 0; y < data.length; y += width) {
+                for (int x = (w-1); x >=0; x-=1) {
+                    buffer[y+x] = data[y+((width-1)-x)];
+                }
+            }
+            data = buffer;
+        }
         if (devFD != 0) {
 
             byte[] buffer = null;
-            switch(pixFormat){
+            switch (pixFormat) {
                 case RGB24:
                     buffer = img2rgb24(data);
                     break;
@@ -52,7 +61,7 @@ public class V4L2Loopback extends VideoOutput{
                     buffer = img2uyvy(data);
                     break;
             }
-            
+
             int countWritten = 0;
             countWritten = CV4l2.INSTANCE.writeData(devFD, buffer, buffer.length);
             if (countWritten != buffer.length) {
@@ -60,7 +69,6 @@ public class V4L2Loopback extends VideoOutput{
             }
         }
     }
-
 
     public interface CV4l2 extends Library {
 
@@ -74,5 +82,3 @@ public class V4L2Loopback extends VideoOutput{
         int writeData(int device, byte[] buffer, int length);
     }
 }
-
-
