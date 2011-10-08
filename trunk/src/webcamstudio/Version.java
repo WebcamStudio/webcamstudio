85ff72d1a07a7f6c707a40bfd869b5159014cc2d
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2011-10-08 15:10:07 +0000

    Increased version number

--- trunk/src/webcamstudio/Version.java
+++ trunk/src/webcamstudio/Version.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  * @author pballeux
  */
 public class Version {
-    public final static String version = "0.57 beta 2";
+    public final static String version = "0.57 beta 3";
     private String build = "";
     public Version(){
         java.io.DataInput di = new java.io.DataInputStream(getClass().getResourceAsStream("build.txt"));
