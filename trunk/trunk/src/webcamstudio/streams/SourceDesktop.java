e65f67cdcc3e32c194cb734dda9e4bc541618e7d
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-04-15 17:21:55 +0000

    Added text and desktop

---
+++ trunk/trunk/src/webcamstudio/streams/SourceDesktop.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webcamstudio.streams;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import webcamstudio.mixers.Frame;
+import webcamstudio.mixers.MasterFrameBuilder;
+
+/**
+ *
+ * @author patrick
+ */
+public class SourceDesktop extends Stream {
+
+    BufferedImage image = null;
+    boolean playing = true;
+    boolean stop = false;
+    Frame frame = null;
+    Robot robot = null;
+    protected int screenwidth = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+    protected int screenheight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+
+    public SourceDesktop() {
+
+        name = "Desktop";
+        try {
+            robot = new Robot();
+        } catch (AWTException ex) {
+            Logger.getLogger(SourceDesktop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Thread capture = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (1 == 1) {
+                    if (frame != null) {
+                        frame.setOutputFormat(x, y, width, height, opacity, volume);
+                        frame.setZOrder(zorder);
+                        frame.setImage(capture());
+                        image = frame.getImage();
+                    }
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(SourceDesktop.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+        capture.setPriority(Thread.MIN_PRIORITY);
+        capture.start();
+
+    }
+
+    private BufferedImage capture() {
+        int mouseX = (int) java.awt.MouseInfo.getPointerInfo().getLocation().getX() - (captureWidth / 2);
+        int mouseY = (int) java.awt.MouseInfo.getPointerInfo().getLocation().getY() - (captureHeight / 2);
+        BufferedImage img = robot.createScreenCapture(new Rectangle(mouseX, mouseY, captureWidth, captureHeight));
+        return img;
+    }
+
+    @Override
+    public void read() {
+        stop = false;
+        try {
+            frame = new Frame(uuid, image, null);
+            frame.setOutputFormat(x, y, width, height, opacity, volume);
+            frame.setZOrder(zorder);
+            MasterFrameBuilder.register(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+        stop = true;
+        MasterFrameBuilder.unregister(this);
+    }
+
+    @Override
+    public Frame getFrame() {
+        return frame;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    @Override
+    public BufferedImage getPreview() {
+        return image;
+    }
+}
---
+++ trunk/trunk/src/webcamstudio/streams/SourceText.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webcamstudio.streams;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import webcamstudio.mixers.Frame;
+import webcamstudio.mixers.MasterFrameBuilder;
+
+/**
+ *
+ * @author patrick
+ */
+public class SourceText extends Stream {
+
+    BufferedImage image = null;
+    String content = "";
+    boolean playing = true;
+    boolean stop = false;
+    String fontName = Font.MONOSPACED;
+    int color = 0xFFFFFF;
+    Frame frame = null;
+
+    public SourceText(String content) {
+        this.content = content;
+        name = "Text";
+        updateContent(content);
+    }
+    public void setColor(int c){
+        color=c;
+        updateContent(content);
+    }
+    public int getColor(){
+        return color;
+    }
+    public void setFont(String f){
+        fontName=f;
+        updateContent(content);
+    }
+    public String getFont(){
+        return fontName;
+    }
+    public void updateContent(String content) {
+        this.content = content;
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D buffer = image.createGraphics();
+        buffer.setRenderingHint(java.awt.RenderingHints.KEY_TEXT_ANTIALIASING, java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        buffer.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        buffer.setRenderingHint(java.awt.RenderingHints.KEY_RENDERING, java.awt.RenderingHints.VALUE_RENDER_QUALITY);
+        buffer.setRenderingHint(java.awt.RenderingHints.KEY_DITHERING, java.awt.RenderingHints.VALUE_DITHER_ENABLE);
+        buffer.setRenderingHint(java.awt.RenderingHints.KEY_COLOR_RENDERING, java.awt.RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        buffer.setFont(new java.awt.Font(fontName, java.awt.Font.PLAIN, height));
+
+        buffer.setBackground(Color.BLACK);
+        buffer.clearRect(0, 0, width, height);
+        buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, 0F));
+        buffer.setColor(new Color(color));
+        buffer.fillRect(0, 0, width, height);
+        buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, 1F));
+        buffer.drawString(content, 0, height);
+        buffer.dispose();
+        if (frame != null) {
+            frame.setImage(image);
+            frame.setOutputFormat(x, y, width, height, opacity, volume);
+            frame.setZOrder(zorder);
+        }
+    }
+
+    public String getContent(){
+        return content;
+    }
+    @Override
+    public void read() {
+        stop = false;
+        try {
+            updateContent(content);
+            frame = new Frame(uuid, image, null);
+            frame.setOutputFormat(x, y, width, height, opacity, volume);
+            frame.setZOrder(zorder);
+            MasterFrameBuilder.register(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+        stop = true;
+        frame = null;
+        MasterFrameBuilder.unregister(this);
+    }
+
+    @Override
+    public Frame getFrame() {
+        frame.setOutputFormat(x, y, width, height, opacity, volume);
+        frame.setZOrder(zorder);
+        return frame;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    @Override
+    public BufferedImage getPreview() {
+        return image;
+    }
+}
