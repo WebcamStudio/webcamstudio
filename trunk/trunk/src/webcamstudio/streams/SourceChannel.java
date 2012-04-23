a6e93e431c2efd5175be979d26d9e42681227e6f
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-04-23 13:42:42 +0000

    Fixed channels settings on desktop can text

--- trunk/trunk/src/webcamstudio/streams/SourceChannel.java
+++ trunk/trunk/src/webcamstudio/streams/SourceChannel.java
@@ -12,60 +12,84 @@ import webcamstudio.sources.effects.Effect;
  *
  * @author patrick
  */
-public class SourceChannel  {
+public class SourceChannel {
 
     int x = 0;
     int y = 0;
+    int capWidth = 0;
+    int capHeight = 0;
     int width = 0;
     int height = 0;
     int opacity = 0;
     float volume = 0;
-    int zorder= 0;
+    int zorder = 0;
+    String text = "";
+    String font = "";
+    int color = 0;
     String name = "";
     boolean isPlaying = false;
     ArrayList<Effect> effects = new ArrayList<Effect>();
-    
-    private SourceChannel(){
+
+    private SourceChannel() {
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String n){
-        name=n;
+
+    public void setName(String n) {
+        name = n;
     }
-    public static SourceChannel getChannel(String channelName,Stream stream){
+
+    public static SourceChannel getChannel(String channelName, Stream stream) {
         SourceChannel s = new SourceChannel();
         s.x = stream.x;
-        s.y =stream.y;
+        s.y = stream.y;
         s.width = stream.width;
         s.height = stream.height;
         s.opacity = stream.opacity;
         s.effects.addAll(stream.effects);
-        s.volume=stream.volume;
-        s.zorder=stream.zorder;
-        s.name=channelName;
+        s.volume = stream.volume;
+        s.zorder = stream.zorder;
+        s.name = channelName;
         s.isPlaying = stream.isPlaying();
+        s.capHeight = stream.captureHeight;
+        s.capWidth = stream.captureWidth;
+        if (stream instanceof SourceText) {
+            SourceText st = (SourceText)stream;
+            s.text=st.content;
+            s.font=st.fontName;
+            s.color=st.color;
+        }
         return s;
     }
-    
-    
-    public void apply(Stream s){
+
+    public void apply(Stream s) {
         s.x = x;
-        s.y =y;
+        s.y = y;
         s.width = width;
         s.height = height;
         s.opacity = opacity;
         s.effects.clear();
         s.effects.addAll(effects);
-        s.volume=volume;
-        s.zorder=zorder;
-        if (isPlaying){
-            if (!s.isPlaying()){
+        s.volume = volume;
+        s.zorder = zorder;
+        s.captureHeight = capHeight;
+        s.captureWidth = capWidth;
+        if (s instanceof SourceText) {
+            SourceText st = (SourceText)s;
+            st.content=text;
+            st.fontName=font;
+            st.color=color;
+            st.updateContent(text);
+        }
+        if (isPlaying) {
+            if (!s.isPlaying()) {
                 System.out.println("Starting source");
                 s.read();
             }
         } else {
-            if (s.isPlaying()){
+            if (s.isPlaying()) {
                 System.out.println("Stopping source");
                 s.stop();
             }
