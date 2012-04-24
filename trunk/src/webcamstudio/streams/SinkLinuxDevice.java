287b4e91174b47144bddfffc71ea948d8ec4ec24
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-04-24 00:09:56 +0000

    Added detection of the end of a file

--- trunk/src/webcamstudio/streams/SinkLinuxDevice.java
+++ trunk/src/webcamstudio/streams/SinkLinuxDevice.java
@@ -6,8 +6,6 @@ package webcamstudio.streams;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import webcamstudio.exporter.vloopback.V4L2Loopback;
 import webcamstudio.exporter.vloopback.VideoOutput;
 import webcamstudio.mixers.Frame;
