b5a62eaa64926200dfdde71676e79481062e3c4c
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-04-27 15:44:34 +0000

    Improved performances on capture

--- trunk/src/webcamstudio/TESTTCPIP.java
+++ trunk/src/webcamstudio/TESTTCPIP.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import webcamstudio.ffmpeg.FFMPEGRenderer;
+import webcamstudio.streams.SourceDesktop;
 import webcamstudio.streams.SourceMovie;
 import webcamstudio.streams.SourceWebcam;
 
@@ -18,14 +19,14 @@ import webcamstudio.streams.SourceWebcam;
 public class TESTTCPIP {
     public static void main(String[] args){
         File file = new File("/dev/video0");
-        SourceWebcam movie = new SourceWebcam(file);
-        movie.setRate(10);
+        SourceDesktop movie = new SourceDesktop();
+        movie.setRate(30);
         
         movie.read();
         while(movie.isPlaying()){
             movie.getFrame();
             try {
-                Thread.sleep(1000/10);
+                Thread.sleep(1000/1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(TESTTCPIP.class.getName()).log(Level.SEVERE, null, ex);
             }
