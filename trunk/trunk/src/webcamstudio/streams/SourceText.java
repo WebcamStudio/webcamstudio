d6eba5b5d0328212d6bab0eeec0004922e5f91ea
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-04-30 19:10:57 +0000

    Added advance background text

--- trunk/trunk/src/webcamstudio/streams/SourceText.java
+++ trunk/trunk/src/webcamstudio/streams/SourceText.java
@@ -6,6 +6,7 @@ package webcamstudio.streams;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import webcamstudio.mixers.Frame;
@@ -23,7 +24,17 @@ public class SourceText extends Stream {
     boolean stop = false;
     String fontName = Font.MONOSPACED;
     int color = 0xFFFFFF;
+    int bgColor = 0x000000;
     Frame frame = null;
+    float bgOpacity = 1f;
+
+    public enum Shape {
+
+        NONE,
+        RECTANGLE,
+        OVAL
+    }
+    private Shape shape = Shape.NONE;
 
     public SourceText(String content) {
         super();
@@ -31,56 +42,117 @@ public class SourceText extends Stream {
         name = "Text";
         updateContent(content);
     }
+
+    public void setBackgroundOpacity(float o){
+        bgOpacity=o;
+        updateContent(content);
+    }
+    public float getBackgroundOpacity(){
+        return bgOpacity;
+    }
+    public void setBackground(Shape s) {
+        shape = s;
+        updateContent(content);
+    }
+
+    public Shape getBackground() {
+        return shape;
+    }
+
     @Override
-    public void setWidth(int w){
-        width=w;
+    public void setWidth(int w) {
+        width = w;
         updateContent(content);
     }
-    public void setHeight(int h){
-        height=h;
+
+    public void setHeight(int h) {
+        height = h;
         updateContent(content);
     }
-    public void setColor(int c){
-        color=c;
+
+    public void setColor(int c) {
+        color = c;
         updateContent(content);
     }
-    public int getColor(){
+
+    public int getColor() {
         return color;
     }
-    public void setFont(String f){
-        fontName=f;
+
+    public void setBackGroundColor(int bgColor) {
+        this.bgColor = bgColor;
         updateContent(content);
     }
-    public String getFont(){
+
+    public int getBackgroundColor() {
+        return bgColor;
+    }
+
+    public void setFont(String f) {
+        fontName = f;
+        updateContent(content);
+    }
+
+    public String getFont() {
         return fontName;
     }
+
     @Override
-    public void setZOrder(int layer){
-        zorder=layer;
+    public void setZOrder(int layer) {
+        zorder = layer;
         updateContent(content);
     }
+
     public void updateContent(String content) {
         this.content = content;
-        captureWidth=width;
-        captureHeight=height;
-        frame = new Frame(captureWidth,captureHeight,rate);
-        image = new BufferedImage(captureWidth, captureHeight+(captureHeight/5), BufferedImage.TYPE_INT_ARGB);
-        
+        captureWidth = width;
+        captureHeight = height;
+        int textHeight = captureHeight;
+        int textWidth = captureWidth;
+        image = new BufferedImage(captureWidth, captureHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D buffer = image.createGraphics();
+        Font font = new java.awt.Font(fontName, java.awt.Font.PLAIN, textHeight);
+        buffer.setFont(font);
+        FontMetrics fm = buffer.getFontMetrics();
+        textHeight = fm.getHeight();
+        textWidth = fm.stringWidth(content);
+        int fontSize = font.getSize();
+        while ((textHeight > captureHeight || textWidth > captureWidth) && fontSize>1){
+            font = new java.awt.Font(fontName, java.awt.Font.PLAIN, fontSize--);
+            buffer.setFont(font);
+            fm = buffer.getFontMetrics();
+            textHeight = fm.getHeight();
+            textWidth = fm.stringWidth(content);
+            
+        }
+        //System.out.println(textHeight + ", " + textWidth );
+        
         buffer.setRenderingHint(java.awt.RenderingHints.KEY_TEXT_ANTIALIASING, java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         buffer.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         buffer.setRenderingHint(java.awt.RenderingHints.KEY_RENDERING, java.awt.RenderingHints.VALUE_RENDER_QUALITY);
         buffer.setRenderingHint(java.awt.RenderingHints.KEY_DITHERING, java.awt.RenderingHints.VALUE_DITHER_ENABLE);
         buffer.setRenderingHint(java.awt.RenderingHints.KEY_COLOR_RENDERING, java.awt.RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        buffer.setFont(new java.awt.Font(fontName, java.awt.Font.PLAIN, captureHeight));
+        frame = new Frame(captureWidth, captureHeight, rate);
+        
 
         buffer.setBackground(Color.BLACK);
-        buffer.clearRect(0, 0, captureWidth, captureHeight+(captureHeight/5));
-        buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, 0F));
-        buffer.setColor(new Color(color));
-        buffer.fillRect(0, 0, captureWidth, captureHeight+(captureHeight/5));
+        buffer.clearRect(0, 0, captureWidth, captureHeight);
+        switch (shape) {
+            case RECTANGLE:
+                buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, bgOpacity));
+                buffer.setColor(new Color(bgColor));
+                buffer.fillRect(0, 0, captureWidth, captureHeight);
+                break;
+            case OVAL:
+                buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, bgOpacity));
+                buffer.setColor(new Color(bgColor));
+                buffer.fillOval(0, 0, captureWidth, captureHeight);
+                break;
+        }
+
         buffer.setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC, 1F));
-        buffer.drawString(content, 0, captureHeight);
+        buffer.setColor(new Color(color));
+        buffer.drawString(content, (captureWidth-textWidth)/2, (captureHeight/2)+(textHeight/2)-fm.getDescent());
         buffer.dispose();
         frame.setImage(image);
         applyEffects(image);
@@ -91,13 +163,14 @@ public class SourceText extends Stream {
         }
     }
 
-    public String getContent(){
+    public String getContent() {
         return content;
     }
+
     @Override
     public void read() {
         stop = false;
-        playing=true;
+        playing = true;
         try {
             updateContent(content);
             frame.setID(uuid);
@@ -113,7 +186,7 @@ public class SourceText extends Stream {
     @Override
     public void stop() {
         stop = true;
-        playing=false;
+        playing = false;
         frame = null;
         MasterFrameBuilder.unregister(this);
     }
