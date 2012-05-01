2b334ae790c18b6bdda20441d40b160808144712
patrick.balleux <patrick.balleux@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2012-05-01 00:26:34 +0000

    Starting Alpha 2 Fixed QRCode issue

--- trunk/src/webcamstudio/streams/SourceQRCode.java
+++ trunk/src/webcamstudio/streams/SourceQRCode.java
@@ -8,10 +8,10 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import webcamstudio.mixers.Frame;
 
 /**
  *
@@ -22,12 +22,14 @@ public class SourceQRCode extends SourceText {
     public SourceQRCode(String content) {
         super(content);
         name="QRCode";
+        
     }
 
     @Override
     public void updateContent(String content) {
         this.content = content;
         if (content != null && content.length() > 0) {
+            frame = new Frame(captureWidth, captureHeight, rate);
             MultiFormatWriter w = new MultiFormatWriter();
             image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             BitMatrix b;
