dc043eb30b4278faa6e2b11cc2365df8ca36a586
peter.u.boy@gmail.com <peter.u.boy@gmail.com@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2013-03-21 22:04:44 +0000

    [CHANGE] Tree restructuring and cleanup [UPDATE] Updated lib jars [ADDED] Debian packaging support [ADDED] Packaging releases script

---
+++ trunk/src/webcamstudio/Version.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webcamstudio;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pballeux
+ */
+public class Version {
+    public final static String version = "0.60";
+    private String build = "";
+    public Version(){
+        java.io.DataInput di = new java.io.DataInputStream(getClass().getResourceAsStream("build.txt"));
+        try {
+            build = di.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(Version.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public String getBuild(){
+        return build;
+    }
+}
--- trunk/trunk/vloopback/comp.sh
+++
@@ -1,4 +0,0 @@
-gcc -c -fPIC libwebcamstudio.c -o libwebcamstudio.o    
-gcc -shared -Wl -o libwebcamstudio.so.1.0.1  libwebcamstudio.o
-sudo cp libwebcamstudio.so.1.0.1 /usr/lib
-
