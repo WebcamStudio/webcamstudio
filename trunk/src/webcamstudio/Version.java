cdd814821660d6798ada7e2317942762259dadca
peter.u.boy@gmail.com <peter.u.boy@gmail.com@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2013-04-02 23:04:28 +0000

    [RELEASE] New release version: 0.63 [FIXES] Missing libwebcamstudio helper library [FIXES] Splashscreen in the debian java wrapper

--- trunk/src/webcamstudio/Version.java
+++ trunk/src/webcamstudio/Version.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  * @author pballeux
  */
 public class Version {
-    public final static String version = "0.61";
+    public final static String version = "0.63";
     private String build = "";
     public Version(){
         java.io.DataInput di = new java.io.DataInputStream(getClass().getResourceAsStream("build.txt"));
