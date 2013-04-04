018092b0b2edd599a383a8a8d536bd0547c18112
peter.u.boy@gmail.com <peter.u.boy@gmail.com@c1cfa5be-0857-d5ae-99db-0581fcdb390f>
2013-04-04 22:09:22 +0000

    [RELEASE] New release version: 0.64

--- trunk/src/webcamstudio/Version.java
+++ trunk/src/webcamstudio/Version.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  * @author pballeux
  */
 public class Version {
-    public final static String version = "0.63";
+    public final static String version = "0.64";
     private String build = "";
     public Version(){
         java.io.DataInput di = new java.io.DataInputStream(getClass().getResourceAsStream("build.txt"));
